65364f6c7fb164959c221f85b3f10a2e7c20b292
Ivanxdd32 <ivanandresmartinezrios@gmail.com>
2025-05-14 19:10:30 -0500

    se realizo un diseño front-end inicial, ajustable

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,48 @@ function App() {
   
   return (
     <>
+      <div id='contentOrder'>
+        <div>
+          <h1>Calculator</h1>
+          <p id='subtitle'>Sencilla por fuera, poderosa por dentro.</p>
+        </div>
+        <div id='mesa'>
+          <div class="calculadora-display">
+            <div class="expresion">3 × (4 + 5)</div>
+            <div class="resultado">27</div>
+          </div>
+          <div id='grid-botones'>
+            <button>C</button>
+            <button>()</button>
+            <button>%</button>
+            <button>←</button>
+
+            <button class="func">sin</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+
+            <button class="func">cos</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+
+            <button class="func">tan</button>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+
+            <button class="func-orange">log</button>
+            <button>0</button>
+            <button>.</button>
+            <button class="igual">=</button>
+
+            <button class="func-orange">√</button>
+            <button class="func-orange">x</button>
+            <button>+</button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
