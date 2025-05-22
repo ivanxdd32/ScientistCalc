9d311763c7d35d726a0afb2fcd821694d9a4a456
Ivanxdd32 <ivanandresmartinezrios@gmail.com>
2025-05-22 18:07:22 -0500

    refactor: restructure CSS into separate files under Styles folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import './Styles/App.css';
+import './Styles/Header.css';
+import './Styles/Main.css';
+import './Styles/Footer.css';
 import App from './App.jsx';
 import './i18n';
 
